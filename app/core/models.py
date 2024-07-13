e70a141dc697e82356d5a42b3292afbafa837235
Diego Florença <diego.florenca@gmail.com>
2024-07-13 12:16:06 -0700

    class 78: Implement recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,7 @@
 """
 Database models.
 """
-# from django.conf import settings
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -43,3 +43,28 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+    # This is based on the basic Django Model.
+    """
+    The user field is a foreign key to the AUTH_USER_MODEL
+    defined in Django's settings. This means that each
+    instance of the Recipe model is associated with a user.
+    The on_delete=models.CASCADE argument specifies that if
+    the user associated with a recipe is deleted, all the
+    recipes associated with that user will also be deleted.
+    """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return str(self.title)
