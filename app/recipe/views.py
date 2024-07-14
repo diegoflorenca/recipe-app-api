b08d164af2f13a0062621cfc2d9853953a12dbcd
Diego Florença <diegoflorenca@gmail.com>
2024-07-14 13:28:23 -0700

    class 81: Implement recipe listing API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,42 @@
-# from django.shortcuts import render
+"""
+Views for the recipe APIs.
+"""
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs."""
+    # Define the serializer class for the RecipeViewSet.
+    serializer_class = serializers.RecipeSerializer
+    # Set the queryset to retrieve all recipes for the authenticated user.
+    queryset = Recipe.objects.all()
+    """
+    Set the authentication class for the RecipeViewSet.
+    The `TokenAuthentication` class is used to authenticate
+    the user using a token. The token is obtained when the user
+    logs in and is included in the request header.
+    """
+    authentication_classes = [TokenAuthentication]
+    """
+    Set the permission class for the RecipeViewSet.
+    The `IsAuthenticated` class is used to ensure that the user
+    is authenticated before accessing any recipe data.
+    """
+    permission_classes = [IsAuthenticated]
+    """
+    Therefor, the user must have a token in the request header and
+    be authenticated to access recipe data.
+    """
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user."""
+        """
+        We override the `get_queryset` method in the `viewsets.ModelViewSet`
+        class to return only the recipes that belong to the authenticated user.
+        """
+        return self.queryset.filter(user=self.request.user).order_by('-id')
